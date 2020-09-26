a1e2f56ed1248e8e1b98c9b9752da46d453a8136
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-09-26 15:19:47 -0700

    change

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import User_Join_Form from '../components/user-form-component/user-join-form.js'
+import User_Create_Form from '../components/create-game-component/create-form.js'
 
 import Navbar from '../components/navbar/Navbar'
 import Button from '../components/button/Button'
@@ -14,12 +15,14 @@ export default function Home() {
           <title>NiceBreakers</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main className={styles.main}>
+
+        <User_Create_Form></User_Create_Form>
+        {/* <main className={styles.main}>
           <div className={styles.grid}>
             <Button text="Create" />
             <Button text="Join" />
           </div>
-        </main>
+        </main> */}
       </div>
     </div>
   );
