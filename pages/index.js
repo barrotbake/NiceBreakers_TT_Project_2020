fb0b558bb5e419b1503c6bee33b33ee94d25214e
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 13:45:29 -0700

    😚 Pretty printed other stuffs

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,24 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Navbar from '../components/navbar/Navbar'
-import Button from '../components/button/Button'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/navbar/Navbar";
+import Button from "../components/button/Button";
 
 export default function Home() {
   return (
     <div>
-      <Navbar> </Navbar>
+      <Navbar />
       <div className={styles.container}>
         <Head>
           <title>NiceBreakers</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
-
         <main className={styles.main}>
-
           <div className={styles.grid}>
-
-            <Button text="Create"/>
-
-            <Button text="Join"/>
-
+            <Button text="Create" />
+            <Button text="Join" />
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
