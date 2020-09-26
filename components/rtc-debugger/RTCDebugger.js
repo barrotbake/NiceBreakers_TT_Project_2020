16c1b07cb9a8dd285a2d3f7e5be184d4c42e074d
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 12:02:13 -0700

    🤞 Created RTCDebugger component
    Created a test component to test interactions with WebRTC

---
+++ components/rtc-debugger/RTCDebugger.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import RTC from "../../utils/RTC";
+
+const RTCDebugger = (props) => {
+  const rtc = new RTC();
+  rtc.initialize();
+  const rtc = useRTC();
+  /*
+  rtc = {
+    id: "<CURRENT_CLIENT_ID>",
+    lobbyId: "<CURRENT_LOBBY_ID>",
+    moderator: "<CONNECTION_TO_MODERATOR>",
+    lobbyMembers: [] // List of connections to other members
+    join(id) {
+      // Joins via LobbyId
+    }
+    send(id, message) {
+      // Sends a message to someone
+    }
+    broadcast(message) {
+      // Sends a message to all lobbyMembers
+    }
+  };
+  */
+  return (
+    <div>
+    
+      <label>Join ID:</label>
+      <span>{rtc.lobbyId}</span>
+    
+      <label>ID:</label>
+      <input
+        type="text"
+        onChange={event => {
+          rtc.join(event.target.value);
+          event.target.value = "";
+          console.log("JOIN_ATTEMPT", { rtc });
+        }}
+      />
+      
+      <label>Message:</label>
+      <input
+        type="text"
+        onChange={event => {
+          rtc.broadcast(event.target.value);
+          event.target.value = "";
+          console.log("MESSAGE_ATTEMPT", { rtc });
+        }}
+       />
+      
+    </div>
+  );
+}
+
+export default RTCDebugger;
