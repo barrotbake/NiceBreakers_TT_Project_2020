4c05ae8b7e18cf430a0db04e85768c994d1e25f4
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 18:53:39 -0700

    🤯 fixed the bug my dudes

---
+++ constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // WebSocket Event Constants
+  NEXT_ROUND: "next-round", // -> Client
+  ANSWER_QUESTION: "answer-question", // -> Server
+  CREATE_LOBBY: "create-lobby", // -> Server
+  JOIN_LOBBY: "join-lobby", // -> Server
+  SET_INFO: "set-info", // -> Server; Sets the name, prefered pronoun, and name pronunciation of a user
+  ROUND_TYPES: {
+    TWO_TRUTHS_AND_A_LIE: "Two Truths and a Lie",
+  },
+
+  // Miscellaneous Constants
+  HOST: "localhost:3000",
+};
