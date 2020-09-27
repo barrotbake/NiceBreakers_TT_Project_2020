c7ee1f8286228b5cc245ccff8a537591b1b5dbb6
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-09-26 20:15:13 -0700

    short commit

---
+++ components/user-join-lobby-component/user-join-lobby.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './user-join-lobby.module.css'
+import Button from "../button/Button.js";
+import Card from 'react-bootstrap/Card'
+
+
+function User_Join_Lobby() {
+    return(
+       console.log(1)
+    )
+}   
+
+export default User_Join_Lobby;
--- pages/test.js
+++ pages/test.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css'
 import User_Join_Form from '../components/user-form-component/user-join-form.js'
 import User_Create_Form from '../components/create-game-component/create-form.js'
+import Host_Join_Lobby from '../components/host-join-lobby-component/host-join-lobby.js'
+import User_Join_Lobby from '../components/user-join-lobby-component/user-join-lobby.js'
 
 import Navbar from '../components/navbar/Navbar'
 import Button from '../components/button/Button'
@@ -19,7 +21,10 @@ export default function Test() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         
-        <User_Create_Form></User_Create_Form>
+        {/* <Host_Join_Lobby></Host_Join_Lobby> */}
+        {/* <User_Join_Lobby></User_Join_Lobby> */}
+        <User_Join_Form></User_Join_Form>
+
 
       </div>
     </div>
