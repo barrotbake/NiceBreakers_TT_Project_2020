e21d868eb68f376cd96ed35f3ead87a9e882f683
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 18:48:20 -0700

    🎨 removed x and o

--- components/mainContainer/MainContainer.js
+++ components/mainContainer/MainContainer.js
@@ -1,40 +1,52 @@
 import styles from "./mainContainer.module.css";
 
+// const createShapeDoodle = (style) => ({
+//   top: `${Math.floor(Math.random() * innerHeight)}px`,
+//   left: `${Math.floor(Math.random() * innerWidth)}px`,
+//   text: Math.random() > 0.5 ? "X" : "O",
+//   style,
+// });
+
 class MainContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bgShapes: [],
-    };
+  constructor() {
+    super();
+    // this.state = {
+    //   bgShapes: [],
+    // };
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        bgShapes: [
-          ...this.state.bgShapes.slice(1),
-          {
-            top: `${Math.floor(Math.random() * innerHeight)}px`,
-            left: `${Math.floor(Math.random() * innerWidth)}px`,
-            style: styles.shape,
-            text: Math.random() > 0.5 ? "X" : "O",
-          },
-        ],
-      });
-    }, 1000);
-  }
+  // componentDidMount() {
+  //   this.setState({
+  //     bgShapes: new Array(10)
+  //       .fill({})
+  //       .map(() => createShapeDoodle(styles.shape)),
+  //   });
+  //   setInterval(() => {
+  //     this.setState(({ bgShapes }) => {
+  //       if (bgShapes[1] !== undefined) {
+  //         bgShapes[1].style = [styles.shape, styles.hide];
+  //       }
+  //       return {
+  //         bgShapes: [
+  //           ...this.state.bgShapes.slice(1),
+  //           createShapeDoodle(styles.shape),
+  //         ],
+  //       };
+  //     });
+  //   }, 1000);
+  // }
 
   render() {
     return (
       <div className={styles.main}>
         {this.props.children}
-        {this.state.bgShapes.map(({ top, left, style, text }) => {
+        {/* {this.state.bgShapes.map(({ top, left, style, text }) => {
           return (
             <a className={style} style={{ top, left }}>
               {text}
             </a>
           );
-        })}
+        })} */}
       </div>
     );
   }
