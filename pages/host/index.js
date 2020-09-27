19680bd8f4d9d395da07222ede90900121a6f994
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 22:30:50 -0700

    🧠LOBBY🧠WORKS🧠

--- pages/host/index.js
+++ pages/host/index.js
@@ -1,13 +1,48 @@
-import { useRound } from "../../hooks/useSockets";
+import { Component } from "react";
+import io from "socket.io-client";
+import {
+  NEXT_ROUND,
+  ANSWER_QUESTION,
+  CREATE_LOBBY,
+  JOIN_LOBBY,
+  SET_INFO,
+  HOST,
+} from "../../constants";
 
-export default function Host() {
-  const round = useRound();
-  return (
-    <div>
-      <div>HOST</div>
+export default class Host extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      round: {
+        finishBy: Infinity, // timestamp for when question is supposed to end
+        leaders: [], // users ordered by highest score
+        roundType: null, // Two Truths and a Lie, Fun Fact
+        pointsEarned: 0, // How many points earned from the last round
+        roundNumber: 0,
+        totalRounds: 0,
+      },
+    };
+  }
+
+  componentDidMount() {
+    const socket = io.connect(HOST);
+    socket.emit(CREATE_LOBBY);
+
+    socket.on(CREATE_LOBBY, ({ user }) => {
+      console.log(CREATE_LOBBY, { user });
+      this.setState((state) => ({
+        user,
+        round: state.round,
+      }));
+    });
+  }
+
+  render() {
+    return (
       <pre>
-        <code>{JSON.stringify(round, null, 2)}</code>
+        <code>{JSON.stringify(this.state, null, 2)}</code>
       </pre>
-    </div>
-  );
+    );
+  }
 }
