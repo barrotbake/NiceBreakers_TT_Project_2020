d052ae68bdc20a6a893f1a71580b05208efb3a12
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-09-26 23:15:01 -0700

    progress

--- components/host-join-lobby-component/host-join-lobby.js
+++ components/host-join-lobby-component/host-join-lobby.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import styles from './host-join-lobby.module.css'
-import Button from "../button/Button.js";
-import Card from 'react-bootstrap/Card'
+import Button from "../button/Button";
 
 
-function Host_Join_Lobby() {
-    return(
-       console.log(1)
+const Host_Join_Lobby = (props) => {
+    return (
+        <div className={styles.gen_box}>
+
+            <div className={styles.top_box}>
+                <h1 className={styles.top_text}>Player Number</h1>
+                <h2 className={styles.top_text}></h2>
+                {/* props.children */}
+            </div>
+            <br/>
+
+
+            <div className={styles.right_box}>
+                <h1 className={styles.right_top_text}>Room Code</h1>
+                <h2 className={styles.right_sub_text}>T</h2>
+                {/* {props.children} */}
+            </div> 
+            
+            
+        </div>
     )
-}   
+}
 
 export default Host_Join_Lobby;
