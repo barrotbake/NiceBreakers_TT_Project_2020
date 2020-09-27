efe3f3f120acc533120a687ca5eb7fa02b823fa3
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 18:24:00 -0700

    Update MainContainer.js

--- components/mainContainer/MainContainer.js
+++ components/mainContainer/MainContainer.js
@@ -1,36 +1,6 @@
 import styles from "./mainContainer.module.css";
 
-<<<<<<< Updated upstream
 class MainContainer extends React.Component {
-=======
-const BGShape = () => {
-  const pos = {
-    top: Math.floor(Math.random() * window.innerHeight) + "px",
-    left: Math.floor(Math.random() * window.innerWidth) + "px"
-  }
-  return (
-    <a className={styles.shape} style={pos}> {Math.random() > 0.5 ? "X" : "O"} </a>
-  )
-}
-
-// class BGShape extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       top: 50,
-//       left: 50,
-//       style: styles.shape,
-//     }
-//   }
-//   render() {
-//     return (
-//       <a className={this.state.style} style={{top: this.state.top, left: this.state.left}}> {Math.random() > 0.5 ? "X" : "O"} </a>
-//     )
-//   }
-// }
-
-class MainContainer extends React.Component{
->>>>>>> Stashed changes
   constructor(props) {
     super(props);
     this.state = {
