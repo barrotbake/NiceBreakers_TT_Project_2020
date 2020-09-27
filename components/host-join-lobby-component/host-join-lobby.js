710b9dfe07e277e34cd56f5a90678368a839a1ca
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-09-26 23:37:41 -0700

    👨🏻‍💻 implemented host lobby

--- components/host-join-lobby-component/host-join-lobby.js
+++ components/host-join-lobby-component/host-join-lobby.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './host-join-lobby.module.css'
 import Button from "../button/Button";
+import Button2 from 'react-bootstrap/Button';
 
 
 const Host_Join_Lobby = (props) => {
@@ -17,9 +18,26 @@ const Host_Join_Lobby = (props) => {
 
             <div className={styles.right_box}>
                 <h1 className={styles.right_top_text}>Room Code</h1>
-                <h2 className={styles.right_sub_text}>T</h2>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <p className={styles.right_sub_text}>TTTTTT</p>
                 {/* {props.children} */}
+    
+                <br/>
+                <br/>
+                <Button text="Start" className={styles.button}/>
+                {/* <Button2 variant="light" className={styles.button} type="submit" form="makeGame">
+                                Start
+                </Button2> */}
             </div> 
+            
             
             
         </div>
