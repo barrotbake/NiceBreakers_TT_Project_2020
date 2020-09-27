831addcda58f27c56a2e8c363e9ef01de8bb8750
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2020-09-26 18:11:03 -0700

    Update index.js

--- pages/host/index.js
+++ pages/host/index.js
@@ -1,6 +1,13 @@
-import { useRound } from "../../hooks/useSockets";
+// import { useRound } from "../../hooks/useSockets";
 
 export default function Host() {
-  const round = useRound();
-  return <div>HOST</div>;
+  // const round = useRound();
+  return (
+    <div>
+      <div>HOST</div>
+      <pre>
+        <code>{JSON.stringify(round, null, 2)}</code>
+      </pre>
+    </div>
+  );
 }
