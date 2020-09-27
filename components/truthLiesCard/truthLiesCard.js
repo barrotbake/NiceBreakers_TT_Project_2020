22ccb1dd091337692d9c184391619d103d6dbf47
barrotbake <nguyenjacob411@gmail.com>
2020-09-27 02:09:35 -0700

    bare two truths and a lie functionality

--- components/truthLiesCard/truthLiesCard.js
+++ components/truthLiesCard/truthLiesCard.js
@@ -1,15 +1,146 @@
-import styles from './truthLiesCard.module.css'
-import Button from '../button/Button'
 
-const TruthLiesCard = (props) => {
-    return(
-        <div className = {styles.TnFCard}>
-            <p>Which one is the lie?</p>
-            <Button classname = {styles.choices} text = {props.truth1} />
-            <Button classname = {styles.choices} text = {props.lie}/>
-            <Button classname = {styles.choices} text = {props.truth2}/>
-        </div>
-    )
+import styles from './truthLiesCard.module.css'
+
+
+class TruthLiesCard extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            found: false,
+            points: 3,
+            card1: {
+                text: "",
+                type: "truth",
+                color: {"background-color" : "green"},
+            },
+            card2:{
+                text: "",
+                type: "truth",
+                color: styles.choices,
+            },
+            card3:{
+                text: "",
+                type: "truth",
+                color: styles.choices,
+            },
+        }
+        this.handleClickCard1 = this.handleClickCard1.bind(this)
+        this.handleClickCard2 = this.handleClickCard2.bind(this)
+        this.handleClickCard3 = this.handleClickCard3.bind(this)
+    }
+
+    randomizer(){
+        if(Math.floor(Math.random() * 9) > 5 ){
+            this.setState({
+                card1:{
+                    text: this.props.lie,
+                    type: "lie"
+                },
+                card2:{
+                    text: this.props.truth2
+                },
+                card3:{
+                    text: this.props.truth1
+                }
+            })
+        } else if(Math.floor(Math.random() * 9) > 2){
+            this.setState({
+                card1:{
+                    text: this.props.truth2,
+                },
+                card2:{
+                    text: this.props.lie,
+                    type: "lie"
+                },
+                card3:{
+                    text: this.props.truth1
+                }
+            })
+        } else {
+            this.setState({
+                card1:{
+                    text: this.props.truth1,
+                },
+                card2:{
+                    text: this.props.truth2,
+                },
+                card3:{
+                    text: this.props.lie,
+                    type: "lie"
+                }
+            })
+        }
+    }
+    componentDidMount(){
+        this.randomizer()
+    }
+
+    componentDidUpdate(){
+        
+    }
+
+    handleClickCard1(e){
+        if(this.state.card1.type === "lie"){
+            this.setState({found:true})
+            e.target.style.color = 'green'
+            e.target.innerText = 'lie'
+        } else {
+            e.target.style.color = 'red'
+            e.target.innerText = 'truth'
+        }
+    }
+
+    handleClickCard2(e){
+        if(this.state.card2.type === "lie"){
+            this.setState({found:true,})
+            e.target.style.color = 'green'
+            e.target.innerText = 'lie'
+        } else {
+            e.target.style.color = 'red'
+            e.target.innerText = 'truth'
+        }
+    }
+
+    handleClickCard3(e){
+        e.preventDefault();
+        if(this.state.card3.type === "lie"){
+            this.setState({found:true})
+            e.target.style.color = 'green'
+            e.target.innerText = 'lie'
+        } else {
+            e.target.style.color = 'red'
+            e.target.innerText = 'truth'
+        }
+    }
+
+    render() {
+       
+        return(
+            <div className = {styles.TnFCard}>
+                <p className = {styles.prompt}>Which one is the lie?</p>
+                <div>
+                    <div className = {styles.firstTwo}>
+                        <button 
+                        onClick={this.handleClickCard1} 
+                        className = {styles.choices}
+                        >{this.state.card1.text}</button>
+                        <button 
+                        onClick={this.handleClickCard2} 
+                        className = {styles.choices}
+                        >{this.state.card2.text}</button>
+                    </div>
+                    <div>
+                    <div className = {styles.last}>
+                        <button 
+                        onClick={this.handleClickCard3} 
+                        className = {styles.choices}>{this.state.card3.text}</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    
 }
 
 export default TruthLiesCard;
