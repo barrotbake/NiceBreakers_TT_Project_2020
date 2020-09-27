eef473f74ccba98d2e364e3835f170ec4b33b2c1
Samuel Sandoval <sjsandoval1000@gmail.com>
2020-09-26 21:37:55 -0700

    😁 User Lobby Component complete

--- components/user-join-lobby-component/user-join-lobby.js
+++ components/user-join-lobby-component/user-join-lobby.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from './user-join-lobby.module.css'
-import Button from "../button/Button.js";
+import Button from "../button/Button";
 import Card from 'react-bootstrap/Card'
 
 
 function User_Join_Lobby() {
-    return(
-       console.log(1)
+    return (
+        <>
+            <Card className={styles.box_css}>
+                <Card.Body>
+                    <Card.Text className={styles.header_text}>
+                        Waiting for host to start the game...
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <br />
+        </>
     )
-}   
+}
 
 export default User_Join_Lobby;
